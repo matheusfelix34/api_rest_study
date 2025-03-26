00685903dac2629e8d1c9f124a5a5562441f588e
Matheus <mfelix3411@gmail.com>
2025-03-26 13:50:21 -0300

    implementando filtros no search

--- app/Http/Controllers/Api/RealStateSearchController.php
+++ app/Http/Controllers/Api/RealStateSearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\RealState;
+use App\Repository\RealStateRepository;
 use Illuminate\Http\Request;
 
 class RealStateSearchController extends Controller
@@ -19,12 +20,27 @@ class RealStateSearchController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $realState = $this->realState->paginate(10);
+        //$realState = $this->realState->paginate(10);
+
+        $repository = new RealStateRepository($this->realState);
+
+
+         
+        if ($request->has('conditions')) {
+          $repository->selectConditions($request->get('conditions'));
+        }
+      
+    
+        if ($request->has('fields')) {
+        
+          $repository->selectFilter($request->get('fields'));
+         
+        }
 
         return response()->json([
-            'data'=> $realState
+            'data'=> $repository->getResult()->paginate(10)
         ], 200);
     }
 
---
+++ app/Repository/RealStateRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Repository;
+
+
+class RealStateRepository extends AbstractRepository
+{
+   
+}
